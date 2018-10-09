f4d695e606c88251bfc3c2ff79b7f679653dac11
Jesray Garciano <jesray.garciano@gmail.com>
2018-10-09 15:35:18 +0800

    COnfiguring the title, descriptions and navigation bars in configjs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+	title: 'Scotch Vuepress',
+	description: "A demo documentation using VuePress",
+	themeConfig: {
+		nav: [
+			{ text: 'COUNTER', link: '/counter/'},
+			{ text: 'GUIDE', link: '/guide/'},
+		],
+		sidebar: [
+		{
+			title: 'Counter',
+			collapsable: false,
+			children: [
+				'/counter/counter-app'
+			]
+		},
+
+		{
+			title: 'API Guide',
+			collapsable: false,
+			children: [
+				'/guide/guide',
+				'/guide/api'
+			]
+		}
+
+
+		]
+	}
+}
